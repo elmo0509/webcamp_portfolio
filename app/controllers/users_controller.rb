1560fae492eab225e6f286e5f5b3999404e9a8c4
elmo0509 <elmo.boof@gmail.com>
2021-11-13 03:53:13 +0000

    コントローラー作成

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,21 @@
+class UsersController < ApplicationController
+  
+  def show
+  end
+  
+  def edit
+  end
+
+  def update
+  end
+
+  def index
+  end
+  
+  def unsubscribe
+  end
+  
+  def withdraw
+  end
+  
+end

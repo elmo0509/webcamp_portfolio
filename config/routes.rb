18ec5459444fa02669aff87c97fedcf12c41d0d5
elmo0509 <elmo.boof@gmail.com>
2021-11-14 03:52:18 +0000

    ルーティング作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,18 @@
 Rails.application.routes.draw do
+  root to: "homes#top"
+  get "about" => "homes#about"
+  resources :introductions  do
+      resource :favorites, only: [:create, :destroy]
+      resources :introduction_comments, only:[:create, :destroy]
+    end
+  resources "users", only: [:show, :edit, :update, :index]
+  get "users/unsubscribe" => "users#unsubscribe"
+  patch "users/withdraw" => "users#withdraw"
+
+  devise_for :users
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
+  
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end

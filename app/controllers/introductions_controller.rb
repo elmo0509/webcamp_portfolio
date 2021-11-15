e814ba02b9675bc2b3d5706c9d2df6dd77c1db23
elmo0509 <elmo.boof@gmail.com>
2021-11-15 11:37:13 +0000

    introduction_newファイル作成

--- app/controllers/introductions_controller.rb
+++ app/controllers/introductions_controller.rb
@@ -1,24 +1,34 @@
 class IntroductionsController < ApplicationController
-  
+
   def new
+    @introduction = Introduction.new
   end
-  
+
   def create
+    @introduction = Introduction.new(introduction_params)
+    @introduction.user_id = introduction_user.id
+    @introduction.save
+    redirect_to introductions_path
   end
-  
+
   def show
   end
-  
+
   def index
   end
-  
+
   def edit
   end
-  
+
   def update
   end
-  
+
   def destroy
   end
-  
+
+  private
+  def introduction_params
+    params.require(:introduction).permit(:place, :author, :title, :address_prefecture, :address_after_prefecture, :address_building_name, :introduction)
+  end
+
 end
